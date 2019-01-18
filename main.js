46fbff90d7168e2be2244df62bea865f6a6acfcb
shaojunda <shaojunda@gmail.com>
2019-01-18 21:27:38 +0800

    ADD: use touch

--- main.js
+++ main.js
@@ -25,37 +25,72 @@ function drawCircle(x, y, radius) {
 var painting = false
 var lastPoint = { x: undefined, y: undefined }
 
-canvas.onmousedown = function(event) {
-  painting = true
-  var x = event.clientX
-  var y = event.clientY
+// 特性检测
+if(document.body.ontouchstart !== undefined) {
+  canvas.ontouchstart = function(event) {
+    painting = true
+    var x = event.touches[0].clientX
+    var y = event.touches[0].clientY
 
-  if(eraserEnabled) {
-    context.clearRect(x-5, y-5, 10, 10)
-  } else {
-    lastPoint = { x: x, y: y }
-    drawCircle(x, y, 1)
-  }
-}
-
-canvas.onmousemove = function(event) {
-  if(painting) {
-    var x = event.clientX
-    var y = event.clientY
-    var newPoint = {x: x, y: y}
     if(eraserEnabled) {
       context.clearRect(x-5, y-5, 10, 10)
-    }else {
-      drawCircle(x, y, 5)
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-      lastPoint = newPoint
+    } else {
+      lastPoint = { x: x, y: y }
+      drawCircle(x, y, 1)
     }
-
   }
-}
 
-canvas.onmouseup = function(event) {
-  painting = false
+  canvas.ontouchmove = function(event) {
+    if(painting) {
+      var x = event.touches[0].clientX
+      var y = event.touches[0].clientY
+      var newPoint = {x: x, y: y}
+      if(eraserEnabled) {
+        context.clearRect(x-5, y-5, 10, 10)
+      }else {
+        drawCircle(x, y, 5)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+  }
+
+  canvas.ontouchend = function(event) {
+    painting = false
+  }
+} else {
+  canvas.onmousedown = function(event) {
+    painting = true
+    var x = event.clientX
+    var y = event.clientY
+
+    if(eraserEnabled) {
+      context.clearRect(x-5, y-5, 10, 10)
+    } else {
+      lastPoint = { x: x, y: y }
+      drawCircle(x, y, 1)
+    }
+  }
+
+  canvas.onmousemove = function(event) {
+    if(painting) {
+      var x = event.clientX
+      var y = event.clientY
+      var newPoint = {x: x, y: y}
+      if(eraserEnabled) {
+        context.clearRect(x-5, y-5, 10, 10)
+      }else {
+        drawCircle(x, y, 5)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+
+    }
+  }
+
+  canvas.onmouseup = function(event) {
+    painting = false
+  }
 }
 
 function drawLine(x1, y1, x2, y2) {
